a069076aaea11d0a5d45e5027995296fc8190f10
Margarita <Margarita>
2023-11-03 17:15:39 +0300

    feat: add auth api and simple api

--- client/src/shared/api/api.ts
+++ client/src/shared/api/api.ts
@@ -1,6 +1,14 @@
 import axios from 'axios';
+import { LOCAL_STORAGE } from 'shared/const/localstorage';
 
 export const api = axios.create({
     baseURL: __API__,
     headers: {},
+});
+
+export const authApi = axios.create({
+    baseURL: __API__,
+    headers: {
+        Authorization: localStorage.getItem(LOCAL_STORAGE.USER_ID) || -1,
+    },
 });
