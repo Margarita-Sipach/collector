f6534ef11b8b600a9bf16d9492c0ecb690e2d108
Margarita <Margarita>
2023-11-18 21:41:49 +0300

    feat: add api class

--- client/src/shared/api/api.ts
+++ client/src/shared/api/api.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { LOCAL_STORAGE } from 'shared/const/localstorage';
+import { errorHandler } from 'shared/error/errorHandler';
 
 export const api = axios.create({
     baseURL: __API__,
@@ -12,3 +13,56 @@ export const authApi = axios.create({
         Authorization: localStorage.getItem(LOCAL_STORAGE.USER_ID) || -1,
     },
 });
+
+export class API{
+	route: string
+
+	constructor(route: string){
+		this.route = route
+	}
+
+	getIdRoute(id: number){
+		return this.route + id
+	}
+
+	async delete(id: any, clb?: Function){
+		const deleteHandler = async() => {
+			const {data} = await authApi.delete(this.getIdRoute(id))
+			await clb?.(data)
+		}
+		return errorHandler(deleteHandler);
+	}
+
+	async update(newData: any, clb?: Function){
+		const update = async() => {
+			const {data} = await authApi.patch(this.route + newData.id, newData)
+			await clb?.(data)
+		}
+		return errorHandler(update);
+	}
+
+	async getById(id: number, clb?: Function) {
+		const getById = async() => {
+			const {data} = await api.get(this.getIdRoute(id));
+		    await clb?.(data)
+		}
+        return errorHandler(getById);
+    }
+
+	
+    async add(newData: any, clb?: Function) {
+        const add = async () => {
+            const {data} = await authApi.post(this.route, newData);
+            await clb?.(data)
+        };
+        return errorHandler(add);
+    }
+
+    async getAll(query: any = {}, clb?: Function) {
+		const getAll = async() => {
+			const {data} = await authApi.get(this.route, {params: query})
+			await clb?.(data)
+		}
+        return errorHandler(getAll);
+    }
+}
