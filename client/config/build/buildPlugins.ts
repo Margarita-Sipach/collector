9e87d0641da5f97507bbf3843626f4b7a47142f0
Margarita <Margarita>
2023-11-23 21:25:20 +0300

    fix: build locales

--- client/config/build/buildPlugins.ts
+++ client/config/build/buildPlugins.ts
@@ -3,6 +3,8 @@ import HTMLWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
+import CopyWebpackPlugin from "copy-webpack-plugin";
+
 
 export const buildPlugins = ({
     paths, isDev, apiUrl,
@@ -20,6 +22,13 @@ export const buildPlugins = ({
             __IS_DEV__: JSON.stringify(isDev),
             __API__: JSON.stringify(apiUrl),
         }),
+		new CopyWebpackPlugin({
+			patterns: [
+			{
+			  from: paths.locales,
+			  to: paths.buildLocales
+			}
+		]})
     ];
 
     if (isDev) {
